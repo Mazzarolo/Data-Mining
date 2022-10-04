42bbe67cc992d0bc265565ef5b91fcc0960c9f7d
Joao <jdrmazzarolo@outlook.com>
2022-10-04 18:12:19 -0300

    Continuing exercise

--- Aula04/aula04.r
+++ Aula04/aula04.r
@@ -1,2 +1,8 @@
-arqUrl <- "http://www-usr.inf.ufsm.br/~joaquim/UFSM/DM/ds/usr_data.csv"
-download.file(arqUrl, destfile = "./test.csv", method = "curl")
+#arqUrl <- "http://www-usr.inf.ufsm.br/~joaquim/UFSM/DM/ds/usr_data.csv"
+#download.file(arqUrl, destfile = "./test.csv", method = "curl")
+
+a <- c(3, 5, 6, 7, 9, 14, 16, 16, NA, 27, 34, 50, 61)
+b <- c(10, 9, 10, 11, 8, 11, 10, 12, NA, 11, 10, 8, 10)
+c <- c(0, 2, 3, 0, 4, 2, 0, 6, 2, 0, 4, 5, NA, 3, 4, NA, 5)
+
+print(a)
