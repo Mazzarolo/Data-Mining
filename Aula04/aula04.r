f4236f6a30e87d0930a6fc4b4628f83d6ae05120
Joao <jdrmazzarolo@outlook.com>
2022-10-06 17:04:05 -0300

    Finishing linear regression

--- Aula04/aula04.r
+++ Aula04/aula04.r
@@ -5,4 +5,14 @@ a <- c(3, 5, 6, 7, 9, 14, 16, 16, NA, 27, 34, 50, 61)
 b <- c(10, 9, 10, 11, 8, 11, 10, 12, NA, 11, 10, 8, 10)
 c <- c(0, 2, 3, 0, 4, 2, 0, 6, 2, 0, 4, 5, NA, 3, 4, NA, 5)
 
-print(a)
+# a[!complete.cases(a)] <- mean(a, na.rm = TRUE)
+
+library(ggplot2)
+
+x_a <- 1 : length(a)
+
+modelo <- lm(x_a - a)
+
+qplot(x_a, a, data = modelo, geom = c("point", "smooth"))
+
+summary(a)
