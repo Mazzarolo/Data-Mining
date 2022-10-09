5b70aacaae8dd56a56d288fc28546fb71ca10588
Joao <jdrmazzarolo@outlook.com>
2022-10-09 19:03:51 -0300

    Adding some study codes, and finishing the activity of class05

--- Aula05/aula05.r
+++ Aula05/aula05.r
@@ -1,15 +1,21 @@
 x <- read.csv(file = "LD00c.csv")
 
+library(tidyr)
 
-for (i in 1 : ncol(x)) {
-    if (is.numeric(x[, i])) {
-        x[!complete.cases(x[, i]), i] <- mean(x[, i], na.rm = TRUE)
-    }
-}
+# code to complete database using the previous and/or next values
+
+x <- x %>% fill(names(x), .direction = "downup")
+
+# code to complete database using the general mean of each col
+
+# for (i in 1 : ncol(x)) {
+#     if (is.numeric(x[, i])) {
+#         x[!complete.cases(x[, i]), i] <- mean(x[, i], na.rm = TRUE)
+#     }
+# }
 
 for (col in x) {
     if (is.numeric(col)) {
-        col[!complete.cases(col)] <- mean(col, na.rm = TRUE)
         print("Média")
         print(mean(col, na.rm = TRUE))
         print("Máx")
@@ -19,7 +25,8 @@ for (col in x) {
     }
 }
 
-max <- x[1, 2]
+print(x[which.max(x$TempMaxima), 2])
 
+hist(x$TempMinima)
 
 print(head(x))
